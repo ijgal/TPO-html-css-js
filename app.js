e2949c1e12630e3ebb66f48852380e617ac41d07
IG_galante@hotmail.com <i_galante@hotmail.com>
2023-05-16 23:51:40 -0300

    Inclusión js con consumo API

--- app.js
+++ app.js
@@ -197,7 +197,18 @@ function actualizarTotalCarrito(){
 }
 
 
+// Consumo API
 
+var ip = '8.8.4.4'; // Leave blank to lookup current IP address
+var XMLHttp = new XMLHttpRequest();
+XMLHttp.onreadystatechange = function() {
+	if(this.readyState == 4 && this.status == 200) {
+		var ipwhois = JSON.parse(this.responseText);
+		console.log(ipwhois.country + ' ' + ipwhois.flag.emoji); // Output: United States 🇺🇸
+	}
+};
+XMLHttp.open('GET', 'https://ipwho.is/' + ip, true);
+XMLHttp.send();
 
 
 
